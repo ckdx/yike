38f2e440568a26b5ee1e9e6693cc14a90a6176ef
ck <18651017228@163.com>
2018-07-10 17:13:20 +0800

    first day
    这是今日一刻第一次写的内容，主要实现了导航栏隐藏显示的功能

---
+++ scripts/app.js
@@ -0,0 +1,43 @@
+/*
+* @Author: ck
+* @Date:   2018-07-10 15:59:23
+* @Last Modified by:   ck
+* @Last Modified time: 2018-07-10 17:07:44
+*/
+//创建app应用模块
+var yike = angular.module("yike",[]);
+/*
+run方法在模块创建完成之后会直接执行
+ */
+yike.run(["$rootScope",function($rootScope){
+	//给头部a标签绑定单机事件，点击可以实现侧边导航栏的显示或隐藏
+	//给collapse类定义值，通过这个值决定collapse是否有效
+	$rootScope.collapsed=false;
+	$rootScope.toggle=function(){
+		$rootScope.collapsed= !$rootScope.collapsed;
+
+	// 显示或隐藏导航栏标题的内容
+	// 获取导航栏中所有的dd
+	var dds=document.querySelectorAll("dd");
+		// 遍历每一个dd给每一个dd修改样式实现隐藏显示的效果
+		// 显示时候，translate的值为0，collapsed的值为true
+		if($rootScope.collapsed){
+			// 此时显示
+			for(var i=0;i<dds.length;i++){
+				dds[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+				dds[i].style.transitionProperty = 'all';
+				dds[i].style.transitionDelay = '0.2s';
+				dds[i].style.transitionTimingFunction = 'ease-out';
+				dds[i].style.transform = 'translate(0)';
+			}
+		}else{
+			for(var i=0;i<dds.length;i++){
+				dds[i].style.transitionDuration = (dds.length - i + 1) * 0.05 + 's';
+				dds[i].style.transitionProperty = 'all';
+				dds[i].style.transitionDelay = '';
+				dds[i].style.transitionTimingFunction = 'ease-out';
+				dds[i].style.transform = 'translate(-100%)';
+			}
+		}
+	}
+}])
